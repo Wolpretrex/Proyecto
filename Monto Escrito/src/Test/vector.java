a82102fe32e9a45342b078e5c312e26fff1322dc
Alejandro <alejandro.vallejo@utp.edu.co>
2018-08-03 01:56:23 -0500

    Se crea la clase Vector, Y Package TEST , para creación de aplicativo de Monto Escrito

---
+++ Monto Escrito/src/Test/vector.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class vector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CLase del Test de MontoEscrito");	
+	}
+
+}
