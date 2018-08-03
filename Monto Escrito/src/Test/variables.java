d899deb546f3997d99f3b5e96115b34953cfc070
Alejandro <alejandro.vallejo@utp.edu.co>
2018-08-03 13:28:44 -0500

    Se crea una clase, para las variables.

---
+++ Monto Escrito/src/Test/variables.java
@@ -0,0 +1,16 @@
+package Test;
+
+public class variables {
+	
+	//Variables
+	
+	int vector[];
+	int cont;
+	String numero;
+	char contador;
+	
+	public static void main(String[] args) {
+		System.out.print("Clase, que contiene variables");
+	}
+
+}
--- Monto Escrito/src/Test/vector.java
+++ Monto Escrito/src/Test/vector.java
@@ -3,13 +3,6 @@ package Test;
 //Clase Vector
 public class vector {
 	
-	int vector[];
-	int cont;
-	String numero;
-	char contador;
-	
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("CLase del Test de MontoEscrito");	
