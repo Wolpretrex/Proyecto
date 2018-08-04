5768d407ae19102dcb99b167ae8d401cda5d8af8
Alejandro <alejandro.vallejo@utp.edu.co>
2018-08-03 22:29:28 -0500

    Se modifica el main para dejarlo sin uso, y se crea el test para compilacion

---
+++ Monto Escrito/src/Test/vectorTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class vectorTest {
+
+	@Test
+	void testDeUnoADiez() {
+		//Datos esperados
+		//Datos obtenidos
+		//Comparacion assert
+	}
+	
+	@Test
+	void testDeOnceACien() {
+		//Datos esperados
+		//Datos obtenidos
+		//Comparacion assert
+	}
+
+}
