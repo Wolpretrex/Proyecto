3a79619a62694780e3ac03f8ed54e196934d6700
Wolpretrex <alejandro.vallejo@utp.edu.co>
2018-08-19 01:57:12 -0500

    En esta version se modifico todo el proyecto recpectivament ey se agregaron los tes respectivos para el 1 al 9 y de 10 al 99

--- Proyecto/src/MontoEscritoTest.java
+++ Proyecto/src/MontoEscritoTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.swing.JOptionPane;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class MontoEscritoTest {
@@ -7,16 +10,29 @@ class MontoEscritoTest {
 
 	@Test
 	void testDeUnoADiez() {
-		//Datos esperados
+		Montoesc ejecucion = new Montoesc();
+		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(8));
 		//Datos obtenidos
+		String DatosObtenidos = ejecucion.convertir(8);
+		System.out.println(DatosObtenidos);
+		//Datos esperados
+		String DatosEsperados = " OCHO";
 		//Comparacion assert
-	}
-	
+		assertEquals(DatosEsperados, DatosObtenidos);
+	}	
+
+
 	@Test
 	void testDeOnceACien() {
-		//Datos esperados
+		Montoesc ejecucion = new Montoesc();
+		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(8));
 		//Datos obtenidos
+		String DatosObtenidos = ejecucion.convertir(85);
+		System.out.println(DatosObtenidos);
+		//Datos esperados
+		String DatosEsperados = " OCHENTA Y CINCO";
 		//Comparacion assert
+		assertEquals(DatosEsperados, DatosObtenidos);
 	}
 
 }
