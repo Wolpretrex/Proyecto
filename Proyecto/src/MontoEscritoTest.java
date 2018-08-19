b423caac4a0bf01d1201685086e2463bf9224280
Wolpretrex <alejandro.vallejo@utp.edu.co>
2018-08-19 02:10:49 -0500

    Se terminan todas las pruebas y se corre el proyecto correctamente se validan distintas combinaciones con exito en todas, y se modifica el respectivo test con sus respectivos nombres del test y con 0 logica, SE validan pruebas y se modifican variables, Finish

--- Proyecto/src/MontoEscritoTest.java
+++ Proyecto/src/MontoEscritoTest.java
@@ -9,12 +9,11 @@ class MontoEscritoTest {
 
 
 	@Test
-	void testDeUnoADiez() {
+	void testDeUnoANueve() {
 		Montoesc ejecucion = new Montoesc();
 		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(8));
 		//Datos obtenidos
 		String DatosObtenidos = ejecucion.convertir(8);
-		System.out.println(DatosObtenidos);
 		//Datos esperados
 		String DatosEsperados = " OCHO";
 		//Comparacion assert
@@ -23,14 +22,37 @@ class MontoEscritoTest {
 
 
 	@Test
-	void testDeOnceACien() {
+	void testDeDiezaNoventayNueve() {
 		Montoesc ejecucion = new Montoesc();
-		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(8));
+		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(85));
 		//Datos obtenidos
 		String DatosObtenidos = ejecucion.convertir(85);
-		System.out.println(DatosObtenidos);
 		//Datos esperados
 		String DatosEsperados = " OCHENTA Y CINCO";
+		//Comparacion assert
+		assertEquals(DatosEsperados, DatosObtenidos);
+	}
+	
+	@Test
+	void testDeCienaNovecientosNoventayNueve() {
+		Montoesc ejecucion = new Montoesc();
+		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(645));
+		//Datos obtenidos
+		String DatosObtenidos = ejecucion.convertir(645);
+		//Datos esperados
+		String DatosEsperados = "SEISCIENTOS CUARENTA Y CINCO";
+		//Comparacion assert
+		assertEquals(DatosEsperados, DatosObtenidos);
+	}
+	
+	@Test
+	void testDeMilaNueveMilNovecientosNuevayNueve() {
+		Montoesc ejecucion = new Montoesc();
+		//JOptionPane.showMessageDialog(null, new Montoesc().convertir(5845));
+		//Datos obtenidos
+		String DatosObtenidos = ejecucion.convertir(5845);
+		//Datos esperados
+		String DatosEsperados = " CINCO MIL OCHOCIENTOS CUARENTA Y CINCO";
 		//Comparacion assert
 		assertEquals(DatosEsperados, DatosObtenidos);
 	}
